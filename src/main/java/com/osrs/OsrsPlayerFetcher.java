2bf64ed4cd27f9a469ffa5f0e5852617a640948a
eow <yaoeow@gmail.com>
2024-12-20 23:28:26 -0800

    adding fetch options

--- src/main/java/com/osrs/OsrsPlayerFetcher.java
+++ src/main/java/com/osrs/OsrsPlayerFetcher.java
@@ -9,11 +9,25 @@ import java.io.IOException;
 public interface OsrsPlayerFetcher {
     /**
      * Fetches player data for the given RuneScape name (RSN).
+     * Uses default options.
      *
      * @param rsn The RuneScape name of the player to fetch data for
      * @return OsrsPlayer object containing the player's skills and activities
      * @throws IOException if there is an error fetching the player data
      * @throws IllegalArgumentException if the RSN is invalid or player not found
      */
-    OsrsPlayer getPlayerByRsn(String rsn) throws IOException, IllegalArgumentException;
+    default OsrsPlayer getPlayerByRsn(String rsn) throws IOException, IllegalArgumentException {
+        return getPlayerByRsn(rsn, FetchOptions.defaults());
+    }
+
+    /**
+     * Fetches player data for the given RuneScape name (RSN) with specified options.
+     *
+     * @param rsn The RuneScape name of the player to fetch data for
+     * @param options Configuration options for the fetch operation
+     * @return OsrsPlayer object containing the player's skills and activities
+     * @throws IOException if there is an error fetching the player data
+     * @throws IllegalArgumentException if the RSN is invalid or player not found
+     */
+    OsrsPlayer getPlayerByRsn(String rsn, FetchOptions options) throws IOException, IllegalArgumentException;
 } 
